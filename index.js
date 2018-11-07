5376174006b01c2f4fa0d6104ca7dd07777d9962
BlakeZWilson <blakewilson5672gmail.com>
2018-11-07 16:12:35 -0700

    pics working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
-  var elems1 = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems1);
   var elems2 = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems2);
-  const form = document.querySelectorAll('#submit-form')
-  submit-form.addEventListener("click",function(){
+  var instances2 = M.Modal.init(elems2);
+  let form1 = document.getElementById('submit-form')
+  form1.addEventListener("submit", function () {
     event.preventDefault()
+    let form = document.getElementById('email-input').value
+    localStorage.setItem('email', form)
   })
 })
